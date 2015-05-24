465959bb523234dbd02c9c1db0bd4fe7424d0229
ainame <s.namai.09@gmail.com>
2015-05-24 11:56:13 +0900

    don't use default read timeout

--- test/test_mediakit_utils_process_runner.rb
+++ test/test_mediakit_utils_process_runner.rb
@@ -1,29 +1,56 @@
 require 'minitest_helper'
+require 'timeout'
 
 class TestMediakitUtilsProcessRunner < Minitest::Test
   def setup
     @bin = File.join(TestContext.root, 'test/supports/ffmpeg')
-    @runner = Mediakit::Utils::ProcessRunner.new(timeout: 0.3)
     STDOUT.sync = true
     STDERR.sync = true
   end
 
   def teardown
-    @runner = nil
+    @timeout = nil
+  end
+
+  def runner
+    options = {}
+    options[:timeout] = @timeout if @timeout
+    Mediakit::Utils::ProcessRunner.new(options)
+  end
+
+  def test_without_timeout_error
+    begin
+      # no timeout error
+      runner.run(@bin, '--sleep=0.1')
+      # no timeout error wtih output
+      runner.run(@bin, '--sleep=0.1 --progress')
+    rescue Timeout::Error => e
+      error = e
+    ensure
+      assert_nil(error)
+    end
   end
 
   def test_timeout_error
+    @timeout = 0.3
     # error with read timeout
     assert_raises(Timeout::Error) do
-      @runner.run(@bin, '--sleep=0.5')
+      runner.run(@bin, '--sleep=0.5')
     end
 
     # no timeout error wtih output
-    @runner.run(@bin, '--sleep=0.5 --progress')
+    begin
+      runner.run(@bin, '--sleep=0.4 --progress')
+    rescue Timeout::Error => e
+      error = e
+    ensure
+      assert_nil(error)
+    end
   end
 
   def test_return_values
-    out, err, status = @runner.run(@bin, '--sleep=0.1 --progress')
+    @timeout = 0.3
+    out, err, status = runner.run(@bin, '--sleep=0.1 --progress')
     assert(out)
     assert(out.kind_of?(String))
     assert(err.kind_of?(String))
