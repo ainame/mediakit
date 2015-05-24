c7d2dac3d130ffd306e5e540de6528af0d48e5ca
ainame <s.namai.09@gmail.com>
2015-05-25 03:48:49 +0900

    fix up

--- .circle/build_ffmpeg.sh
+++ .circle/build_ffmpeg.sh
@@ -1,9 +1,8 @@
 set -x
 set -e
 if [ ! -e ffmpeg-2.6.3/ffmpeg ]; then
-    sudo apt-get update
-    sudo apt-get -y --force-yes install autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev\
-         libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev\
+    sudo apt-get -y --force-yes install autoconf automake build-essential libass-dev libfreetype6-dev libgpac-dev \
+         libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
          libxcb-xfixes0-dev pkg-config texi2html zlib1g-dev libx264-dev yasm
     wget http://ffmpeg.org/releases/ffmpeg-2.6.3.tar.bz2
     tar xjf ffmpeg-2.6.3.tar.bz2
