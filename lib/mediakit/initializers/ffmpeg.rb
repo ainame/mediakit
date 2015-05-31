310c1ad3a95b0d9fa96f8ac4ec1a06f035b9a76a
ainame <s.namai.09@gmail.com>
2015-05-31 20:05:19 +0900

    suppress initializers logging

--- lib/mediakit/initializers/ffmpeg.rb
+++ lib/mediakit/initializers/ffmpeg.rb
@@ -50,7 +50,7 @@ module Mediakit
 
         def raw_items
           options = Mediakit::FFmpeg::Options.new(Mediakit::FFmpeg::Options::GlobalOption.new('codecs' => true))
-          output, _, _ = @command.run(options)
+          output, _, _ = @command.run(options, logger: nil)
           return [] if output.nil? || output.empty?
           output.split(DELIMITER_FOR_CODECS)[1].each_line.to_a
         end
@@ -92,7 +92,7 @@ module Mediakit
 
         def raw_items
           options = Mediakit::FFmpeg::Options.new(Mediakit::FFmpeg::Options::GlobalOption.new('formats' => true))
-          output, _, _ = @command.run(options)
+          output, _, _ = @command.run(options, logger: nil)
           return [] if output.nil? || output.empty?
           output.split(DELIMITER_FOR_FORMATS)[1].each_line.to_a
         end
@@ -129,7 +129,7 @@ module Mediakit
 
         def raw_items
           options = Mediakit::FFmpeg::Options.new(Mediakit::FFmpeg::Options::GlobalOption.new('decoders' => true))
-          output, _, _ = @command.run(options)
+          output, _, _ = @command.run(options, logger: nil)
           return [] if output.nil? || output.empty?
           output.split(DELIMITER_FOR_CODER)[1].each_line.to_a
         end
@@ -179,7 +179,7 @@ module Mediakit
 
         def raw_items
           options = Mediakit::FFmpeg::Options.new(Mediakit::FFmpeg::Options::GlobalOption.new('encoders' => true))
-          output, _, _ = @command.run(options)
+          output, _, _ = @command.run(options, logger: nil)
           return [] if output.nil? || output.empty?
           output.split(DELIMITER_FOR_CODER)[1].each_line.to_a
         end
