9bda1bd7916295cd1d61ae73bc7d991f92edc571
ainame <s.namai.09@gmail.com>
2015-07-10 09:41:13 +0000

    delete init

--- lib/mediakit/railtie.rb
+++ lib/mediakit/railtie.rb
@@ -1,7 +1,7 @@
 module Mediakit
   class Railtie < ::Rails::Railtie
     initializer('mediakit.initialize_ffmpeg') do
-      Mediakit::FFmpeg.create.init
+      Mediakit::FFmpeg.create
     end
   end
 end
